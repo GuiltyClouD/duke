5f32e19e2947fe2c4ac3c1129238632f75b6f095
GC <e0493726@u.nus.edu>
2021-11-13 12:30:00 +0800

    Create Event.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,16 @@
+//level 4
+
+public class Event extends Task {
+
+    protected String by;
+
+    public Event(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (by: " + by + ")";
+    }
+}
