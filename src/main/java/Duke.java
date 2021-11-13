6b65787e0d2551da8b38ff07d11576723f77a4b8
GC <e0493726@u.nus.edu>
2021-11-13 19:02:55 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException{
 /*        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -23,34 +23,29 @@ public class Duke {
 
         while (true)
         {
-            //level 1 and 2
-            Scanner in = new Scanner(System.in);
-            line = in.nextLine();
-            if (line.equals("bye"))
-            {
-                System.out.print(bye);
-                break;
-            }
-            else if (line.equals("list"))
-            {
-                System.out.print("    ____________________________________________________________\n"
-                        +    "     Here are the tasks in your list:\n");
-                for (int i = 1; i <= taskList.size(); i++)
-                {
-                    System.out.print("     " + i + ". " + taskList.get(i - 1) + "\n");
+            try {
+                //level 1 and 2
+                Scanner in = new Scanner(System.in);
+                line = in.nextLine();
+                if (line.equals("bye")) {
+                    System.out.print(bye);
+                    break;
+                } else if (line.equals("list")) {
+                    System.out.print("    ____________________________________________________________\n"
+                            + "     Here are the tasks in your list:\n");
+                    for (int i = 1; i <= taskList.size(); i++) {
+                        System.out.print("     " + i + ". " + taskList.get(i - 1) + "\n");
+                    }
+                    System.out.print("    ____________________________________________________________\n");
+                } else if (line.startsWith("done") && Integer.parseInt(line.substring(5)) <= taskList.size()) {
+                    int tempLineNumber = Integer.parseInt(line.substring(5));
+                    taskList.get(tempLineNumber - 1).markAsDone();
+                    System.out.print("    ____________________________________________________________\n"
+                            + "     Nice! I've marked this task as done:\n");
+                    System.out.print("     " + "[" + taskList.get(tempLineNumber - 1).getStatusIcon() + "] "
+                            + taskList.get(tempLineNumber - 1).getDescription() + "\n");
+                    System.out.print("    ____________________________________________________________\n");
                 }
-                System.out.print("    ____________________________________________________________\n");
-            }
-            else if (line.startsWith("done") && Integer.parseInt(line.substring(5)) <= taskList.size())
-            {
-                int tempLineNumber = Integer.parseInt(line.substring(5));
-                taskList.get(tempLineNumber - 1).markAsDone();
-                System.out.print("    ____________________________________________________________\n"
-                        +    "     Nice! I've marked this task as done:\n");
-                System.out.print("     " + "[" + taskList.get(tempLineNumber-1).getStatusIcon() + "] "
-                        + taskList.get(tempLineNumber-1).getDescription() + "\n");
-                System.out.print("    ____________________________________________________________\n");
-            }
             /* else
             {
                 Task newTask = new Task(line);
@@ -59,38 +54,41 @@ public class Duke {
                         + "     Added: " + newTask.getDescription()
                         + "\n" + "    ____________________________________________________________\n");
             } */
-            else if (line.startsWith("todo"))
+                else if (line.startsWith("todo")) {
+                    String newTask = line.substring(5);
+                    taskList.add(new toDo(newTask));
+                    System.out.print("    ____________________________________________________________\n"
+                            + "     Got it. I've added this task:\n");
+                    System.out.print("     " + taskList.get(taskList.size() - 1) + "\n");
+                    System.out.print("     Now you have " + taskList.size() + " tasks in the list.\n");
+                    System.out.print("    ____________________________________________________________\n");
+                } else if (line.startsWith("deadline")) {
+                    String[] newDeadline = line.substring(9).split("/");
+                    String deadlineTask = newDeadline[0];
+                    String deadlineDate = newDeadline[1].substring(3);
+                    taskList.add(new Deadline(deadlineTask, deadlineDate));
+                    System.out.print("    ____________________________________________________________\n"
+                            + "     Got it. I've added this task:\n");
+                    System.out.print("     " + taskList.get(taskList.size() - 1) + "\n");
+                    System.out.print("     Now you have " + taskList.size() + " tasks in the list.\n");
+                    System.out.print("    ____________________________________________________________\n");
+                } else if (line.startsWith("event")) {
+                    String[] newEvent = line.substring(6).split("/");
+                    String eventTask = newEvent[0];
+                    String eventDate = newEvent[1].substring(3);
+                    taskList.add(new Event(eventTask, eventDate));
+                    System.out.print("    ____________________________________________________________\n"
+                            + "     Got it. I've added this task:\n");
+                    System.out.print("     " + taskList.get(taskList.size() - 1) + "\n");
+                    System.out.print("     Now you have " + taskList.size() + " tasks in the list.\n");
+                    System.out.print("    ____________________________________________________________\n");
+                } else {
+                    System.out.print("    ____________________________________________________________\n");
+                    throw new DukeException("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e)
             {
-                String newTask = line.substring(5);
-                taskList.add(new toDo(newTask));
-                System.out.print("    ____________________________________________________________\n"
-                        +    "     Got it. I've added this task:\n");
-                System.out.print("     " + taskList.get(taskList.size() - 1) + "\n");
-                System.out.print("     Now you have " + taskList.size() + " tasks in the list.\n");
-                System.out.print("    ____________________________________________________________\n");
-            }
-            else if (line.startsWith("deadline"))
-            {
-                String[] newDeadline = line.substring(9).split("/");
-                String deadlineTask = newDeadline[0];
-                String deadlineDate = newDeadline[1].substring(3);
-                taskList.add(new Deadline(deadlineTask, deadlineDate));
-                System.out.print("    ____________________________________________________________\n"
-                        +    "     Got it. I've added this task:\n");
-                System.out.print("     " + taskList.get(taskList.size() - 1) + "\n");
-                System.out.print("     Now you have " + taskList.size() + " tasks in the list.\n");
-                System.out.print("    ____________________________________________________________\n");
-            }
-            else if (line.startsWith("event"))
-            {
-                String[] newEvent = line.substring(6).split("/");
-                String eventTask = newEvent[0];
-                String eventDate = newEvent[1].substring(3);
-                taskList.add(new Event(eventTask, eventDate));
-                System.out.print("    ____________________________________________________________\n"
-                        +    "     Got it. I've added this task:\n");
-                System.out.print("     " + taskList.get(taskList.size() - 1) + "\n");
-                System.out.print("     Now you have " + taskList.size() + " tasks in the list.\n");
+                System.out.println(e);
                 System.out.print("    ____________________________________________________________\n");
             }
         }
